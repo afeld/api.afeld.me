fdd16dab291e71c4d4d07fe9cf377b385220dd4d
Aidan Feldman <aidan.feldman@gmail.com>
2013-02-10 01:16:19 -0500

    render objects in <ul>s (broken)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,39 @@
-(function($) {
+(function($){
+  // taken from Underscore
+  function isObject(obj){
+    return obj === Object(obj);
+  }
+
+  function renderHash(hash, $el){
+    var $ul = $('<ul>'),
+      val, li, $li;
+
+    $ul.appendTo($el);
+
+    for (var key in hash){
+      val = hash[key];
+      li = '<li>';
+      if (isObject(val)){
+        li += '<a class="key" href="#' + key + '">' + key + '</a>';
+      } else {
+        li += '<span class="key">' + key + '</span>: <span class="val">' + val + '</span>';
+      }
+      li += '</li>';
+
+      $li = $(li);
+      $li.appendTo($ul);
+    }
+  }
+
+  function renderArray(ary, $el){
+
+  }
+
+  function renderData(data, $el){
+    renderHash(data, $el);
+  }
+
   $.fn.jsonPanes = function(data){
-    var $el = $(this);
-    $el.html(JSON.stringify(data));
+    renderData(data, $(this));
   };
 })(jQuery);
