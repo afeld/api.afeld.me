7f89db4dc6743e13ae2b4e0f0a5b663782e818bc
Aidan Feldman <aidan.feldman@gmail.com>
2013-02-18 02:07:16 -0500

    refactor to use jquery helpers

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,28 +1,20 @@
 (function($){
-  // taken from Underscore
-  function isObject(obj){
-    return obj === Object(obj);
-  }
-
   function renderHash(hash, $el){
-    var $ul = $('<ul>'),
-      val, li, $li;
-
+    var $ul = $('<ul>');
     $ul.appendTo($el);
 
-    for (var key in hash){
-      val = hash[key];
-      li = '<li>';
-      if (isObject(val)){
+    $.each(hash, function(key, val){
+      var li = '<li>';
+      if ($.isPlainObject(val)){
         li += '<a class="key" href="#' + key + '">' + key + '</a>';
       } else {
         li += '<span class="key">' + key + '</span>: <span class="val">' + val + '</span>';
       }
       li += '</li>';
 
-      $li = $(li);
+      var $li = $(li);
       $li.appendTo($ul);
-    }
+    });
   }
 
   function renderArray(ary, $el){
