99a99297506cf429b73db748429ef28f61cd91b8
Aidan Feldman <aidan.feldman@gmail.com>
2013-06-08 14:17:52 -0400

    make site crawlable - closes #14

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/respond_to'
+require 'json'
 
 Sinatra::Application.register Sinatra::RespondTo
 enable :logging
@@ -17,6 +18,43 @@ configure do
 end
 
 PROFILE_STR = File.read('./views/index.json').freeze
+PROFILE_HSH = JSON.parse(PROFILE_STR).freeze
+
+helpers do
+  def to_html(val)
+    case val
+    when Array
+      to_ul(val)
+    when Hash
+      to_dl(val)
+    else
+      val
+    end
+  end
+
+  def to_dl(hash)
+    out = "<dl>\n"
+    hash.each do |key, val|
+      out << "<dt>#{key}</dt>\n"
+      dd = to_html(val)
+      out << "<dd>#{dd}</dd>\n"
+    end
+    out << "</dl>\n"
+
+    out
+  end
+
+  def to_ul(array)
+    out = "<ul>\n"
+    array.each do |item|
+      li = to_html(item)
+      out << "<li>#{li}</li>\n"
+    end
+    out << "</ul>\n"
+
+    out
+  end
+end
 
 
 get '/stylesheets/application' do
