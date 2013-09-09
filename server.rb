f94970b536c677b27de6d248a0d07abd05758878
Aidan Feldman <aidan.feldman@gmail.com>
2013-09-09 08:50:31 -0700

    add github job description

--- server.rb
+++ server.rb
@@ -61,9 +61,13 @@ get '/stylesheets/application' do
   scss :application
 end
 
-get %r{/(index)?} do
+get %r{^/(index)?$} do
   respond_to do |wants|
     wants.html { erb :index }      # => views/posts.html.haml, also sets content_type to text/html
     wants.json { PROFILE_STR }       # => views/posts.rss.haml, also sets content_type to application/rss+xml
   end
+end
+
+get '/github_desc' do
+  erb :github_desc
 end
