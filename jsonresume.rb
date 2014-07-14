c804ef81175a5a5871588717cd4094af441ee85f
Aidan Feldman <aidan.feldman@gmail.com>
2014-07-14 17:55:20 -0400

    add work and teaching to jsonresume

--- jsonresume.rb
+++ jsonresume.rb
@@ -1,8 +1,15 @@
+# http://jsonresume.org
+#
+# npm install -g resume-cli
+#
+
+require 'date'
 require 'json'
 
 json = JSON.parse(File.read('views/index.json'))
 
 name = json['name'].split
+um = json['education'][0]
 
 resume = {
   bio: {
@@ -25,18 +32,38 @@ resume = {
       github: json['profiles']['github']['username']
     }
   },
-  work: []
+  work: [],
+  education: [
+    {
+      institution: um['school'],
+      # TODO PR to support multiple degrees
+      area: 'BSE Computer Science Engineering',
+      studyType: 'BFA Dance',
+      startDate: um['start_date'],
+      endDate: um['end_date']
+    }
+  ]
 }
 
-json['employment']['coding'].each do |empl|
-  resume[:work] << {
+jobs = json['employment']['coding'] + json['employment']['teaching']
+
+# sort by jobs by last ended, then by last started, in descending order
+jobs.sort_by! do |job|
+  end_date = job['end_date'] || Time.now.strftime('%Y-%m-%d')
+  [end_date, json['start_date']]
+end
+jobs.reverse!
+
+jobs.each do |empl|
+  hsh = {
     company: empl['organization'],
     position: empl['title'],
     website: empl['url'],
     startDate: empl['start_date'],
-    endDate: empl['end_date'],
     summary: empl['description']
   }
+  hsh[:endDate] = empl['end_date'] if empl['end_date']
+  resume[:work] << hsh
 end
 
 File.open('resume.json', 'w') do |file|
