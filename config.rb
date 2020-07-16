6563739e9698a1989add835669f40ba8a3eff82a
Aidan Feldman <aidan.feldman@gmail.com>
2020-07-16 05:09:01 -0400

    remove tests
    Not testing anything the site build wouldn't catch.

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-activate :livereload unless ENV['RACK_ENV'] == 'test'
+activate :livereload
 
 # Layouts
 # https://middlemanapp.com/basics/layouts/
