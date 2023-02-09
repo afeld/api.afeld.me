80d25adc49479c3834b2ae428d39f39c21c4f4f6
Aidan Feldman <aidan.feldman@gmail.com>
2023-02-09 03:27:02 -0500

    hide the navigation when clicking outside of it

--- source/resume/js/resume.js
+++ source/resume/js/resume.js
@@ -1,4 +1,11 @@
-const $toc = $("#toc");
+const $topnav = $(".topnav");
+const $toc = $topnav.find("#toc");
+
+// hide the nav when clicking outside the nav drawer
+$(document.body).click(() => $toc.hide());
+$topnav.click((event) => event.stopPropagation());
+
+// hamburger menu
 $(".icon").click(() => $toc.toggle());
 
 $(() => {
