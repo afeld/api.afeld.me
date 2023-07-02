8c9f032c0a830d1cf03b25a373054e3d9fc0194e
Aidan Feldman <aidan.feldman@gmail.com>
2023-07-01 18:16:55 -0700

    fix/ignore incorrect tenses in job descriptions

--- tense.py
+++ tense.py
@@ -23,7 +23,41 @@ def all_past(text: str):
     return all(detect_past_sentence(s) for s in sentences)
 
 
+# there is probably a way to avoid these false positives, but just skip them explicitly for now
+IGNORE = [
+    "self-employed - VoteAmerica",
+    "Technology Transformation Services (TTS)",
+    "Technology Transformation Services (TTS) - FAS Systems Governance Committee (FSGC)",
+    "Technology Transformation Services (TTS) - Mac Working Group",
+    "Upsolve",
+    "Brooklyn Rail",
+    "xD",
+    "xD - Recalls",
+    "NYC Planning Labs - Discovery",
+    "18F",
+    "18F - eQIP",
+    "18F - DevSecOps Working Group",
+    "18F - CIO Liaison",
+    "18F - Project Boise",
+    "18F - ATO Sprinting/Streamlining Team",
+    "18F - cloud.gov",
+    "18F - OpenControl / Compliance Masonry",
+    "GitHub",
+    "Artsy",
+    "American Express Publishing",
+    "EmpireConf",
+    "New York University (NYU) - School of Professional Studies (SPS)",
+    "New York University (NYU) - Interactive Telecommunications Program (ITP)",
+    "City University of New York (CUNY)",
+    "General Assembly",
+    "Hacker Hours",
+]
+
+
 def check_tense(end_date: str | None, desc: str, role: str):
+    if role in IGNORE:
+        return
+
     if end_date:
         if not all_past(desc):
             print(f"{role} has a present-tense sentence")
@@ -44,5 +78,6 @@ with open("data/resume.json") as f:
 
             responsibilities = job.get("responsibilities", [])
             for responsibility in responsibilities:
-                role = f"{org} - {responsibility['group']}"
+                group = responsibility["group"] or responsibility["title"]
+                role = f"{org} - {group}"
                 check_tense(end_date, responsibility["description"], role)
